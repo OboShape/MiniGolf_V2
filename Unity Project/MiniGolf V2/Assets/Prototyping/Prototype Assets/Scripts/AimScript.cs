7500e69a223f37a13c43b7cedcd0771139d0e552
OboShape <dar_mcbain@yahoo.co.uk>
2017-03-29 20:18:14 +0100

    Mouse Pointer Tracking
    Using raycasting and mouse input to get and show aiming direction

--- Unity Project/MiniGolf V2/Assets/Prototyping/Prototype Assets/Scripts/AimScript.cs
+++ Unity Project/MiniGolf V2/Assets/Prototyping/Prototype Assets/Scripts/AimScript.cs
@@ -9,13 +9,12 @@ public class AimScript : MonoBehaviour {
 	private AimArrowSprite m_arrowSprite;
 	private Camera m_viewCam;
 
-	[Range(1f, 5f)] public float aimPlaneScale;
-
-
 	void Start()
 	{
 		m_arrowSprite = GetComponentInChildren<AimArrowSprite>();
 		m_viewCam = Camera.main;
+
+		Plane newPlane = new Plane(
 	}
 
 
@@ -24,18 +23,16 @@ public class AimScript : MonoBehaviour {
 		// create ray
 		Ray ray = m_viewCam.ScreenPointToRay(Input.mousePosition);
 		// create the aim plane
-		Plane groundPlane = new Plane (
-			// just need to declare 3 corner points to create the plane, as it iterpolates the 4th corner.
-			// doing it this way, creates the plane dead centre of the ball
-			new Vector3(transform.position.x + 10f, transform.position.y, transform.position.z-10f),
-			new Vector3(transform.position.x - 10f, transform.position.y, transform.position.z-10f),
-			new Vector3(transform.position.x + 10f, transform.position.y, transform.position.z+10f));
+		Plane groundPlane = new Plane(Vector3.up, transform.position);
+
 		// used for raycasting out, logs hit distance
 		float rayDistance;
 
 		if(groundPlane.Raycast(ray, out rayDistance))
 		{
 			Vector3 point = ray.GetPoint(rayDistance);
+			// so i can see the ray
+			Debug.DrawLine(ray.origin, point,Color.red);
 			// since the plane is at same Y level as ball, set the aim arrow to look at the hit point. 
 			// (always uses the transforms local forward vector, this way the arrow is always flat)
 			transform.LookAt(point);
@@ -44,5 +41,10 @@ public class AimScript : MonoBehaviour {
 
 	}
 
+	void OnDrawGizmos()
+	{
+		Gizmos.DrawLine(new Vector3(transform.position.x + 1f, transform.position.y, transform.position.z-1f),
+								new Vector3(transform.position.x - 1f, transform.position.y, transform.position.z-1f)); 
+	}
 }
 
